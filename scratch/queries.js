28abc02369b95cf9a61a702824eec7cb7b62c41d
Janet Leon <leon.irenejanet@gmail.com>
2018-09-11 13:13:59 -0700

    updated scratch queries file to have fresh data inserted with chained promises

--- scratch/queries.js
+++ scratch/queries.js
@@ -1,20 +1,44 @@
 'use strict';
-
 const knex = require('../knex');
+const jsonNotes = require ('../db/notes.json');
 
-let searchTerm = 'gaga';
 knex
-  .select('notes.id', 'title', 'content')
-  .from('notes')
-  .modify(queryBuilder => {
-    if (searchTerm) {
-      queryBuilder.where('title', 'like', `%${searchTerm}%`);
-    }
+  .schema
+  .dropTableIfExists('notes')
+  .then((results)=>{
+    return knex.schema.createTable('notes', (table) => {
+      table.increments('id');
+      table.string('title').notNullable();
+      table.string('content', 1000);
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+    });
+  }).then (results => {
+    knex('notes')
+      .insert(jsonNotes)
+      .debug(false)
+      .then(results => console.log ('notes created'));
+
   })
-  .orderBy('notes.id')
   .then(results => {
-    console.log(JSON.stringify(results, null, 2));
-  })
-  .catch(err => {
-    console.error(err);
+    let searchTerm = 'gaga';
+    knex
+      .select('notes.id', 'title', 'content')
+      .from('notes')
+      .modify(queryBuilder => {
+        if (searchTerm) {
+          queryBuilder.where('title', 'like', `%${searchTerm}%`);
+        }
+      })
+      .orderBy('notes.id')
+      .then(results => {
+        console.log(JSON.stringify(results, null, 2));
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    
+
   });
+  
+
+
