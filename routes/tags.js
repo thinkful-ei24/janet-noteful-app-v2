309b9e216322aaab02e37230fdd9b93dc268c8d6
Janet Leon <leon.irenejanet@gmail.com>
2018-09-13 09:40:26 -0700

    created a get by ID endpoint on TAGS router

--- routes/tags.js
+++ routes/tags.js
@@ -4,10 +4,23 @@ const express = require('express');
 const knex = require('../knex');
 const router = express.Router();
 
-
+//========GET ALL TAGS=============
 router.get('/', (req,res,next) =>{
   knex.select()
     .from('tags')
+    .then(results=> res.json(results))
+    .catch(err => {
+      next(err);
+    });
+});
+
+//========GET TAGS BY ID=============
+router.get('/:id', (req,res,next) =>{
+  const tagID = req.params.id;
+
+  knex.select()
+    .from('tags')
+    .where({id: `${tagID}`})
     .then(results=> res.json(results))
     .catch(err => {
       next(err);
