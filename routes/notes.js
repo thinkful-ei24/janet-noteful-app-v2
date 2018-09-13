8de3123f7123b54e5d462f59d0cb2310fa81f86b
Janet Leon <leon.irenejanet@gmail.com>
2018-09-13 16:29:51 -0700

    updated put function

--- routes/notes.js
+++ routes/notes.js
@@ -130,23 +130,15 @@ router.post('/', (req, res, next) => {
 
 router.put('/:id', (req, res, next) => {
   const noteId = req.params.id;
-  const { title, content, folderID } = req.body;
+  const { title, content, folderId } = req.body;
+  const tags = req.body.tags || [];
+
 
   const updateItem = {
     title: title,
     content: content,
-    folder_id: (folderID) ? folderID : null
+    folder_id: (folderId) ? folderId : null
   };
-
-  //   /***** Never trust users - validate input *****/
-  //   const updateObj = {};
-  //   const updateableFields = ['title', 'content', 'folderId'];
-
-  //   updateableFields.forEach(field => {
-  //     if (field in req.body) {
-  //       updateObj[field] = req.body[field];
-  //     }
-  //   });
 
   /***** Never trust users - validate input *****/
   if (!updateItem.title) {
@@ -158,24 +150,41 @@ router.put('/:id', (req, res, next) => {
   knex('notes')
     .update(updateItem)
     .where('id', noteId)
-    .then((results) => {
-      // Using the noteId, select the note and the folder info
-      return knex.select('notes.id', 'title', 'content', 'folder_id as folderId', 'folders.name as folderName')
+    .then(()=>{
+      knex.select()
+        .from('notes_tags')
+        .where({'note_id': `${noteId}`})
+        .del();
+   
+    })
+    .then(() => {
+      // Insert related tags into notes_tags table
+      const tagsInsert = tags.map(tagId => ({ note_id: noteId, tag_id: tagId }));
+      return knex.insert(tagsInsert).into('notes_tags');
+    })
+    .then(() => {
+      // Select the new note and leftJoin on folders and tags
+      return knex.select('notes.id', 'title', 'content',
+        'folders.id as folder_id', 'folders.name as folderName',
+        'tags.id as tagId', 'tags.name as tagName')
         .from('notes')
         .leftJoin('folders', 'notes.folder_id', 'folders.id')
+        .leftJoin('notes_tags', 'notes.id', 'notes_tags.note_id')
+        .leftJoin('tags', 'tags.id', 'notes_tags.tag_id')
         .where('notes.id', noteId);
     })
-    .then((results) => {
-      // if (result) {
-      res.json(results[0]);
-      // } 
-      // else {
-      //   next();
-      // }
+    .then(result => {
+      if (result) {
+        // Hydrate the results
+        const hydrated = hydrateNotes(result)[0];
+        // Respond with a location header, a 201 status and a note object
+        res.json(hydrated);
+      } else {
+        next();
+      }
     })
-    .catch(err => {
-      next(err);
-    });
+    .catch(err => next(err));
+
 });
 
 //============ Delete an item==========================
