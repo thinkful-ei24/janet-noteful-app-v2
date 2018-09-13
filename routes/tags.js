9fee4f22dd4741a8392836f6d7f4eebb19a57d28
Janet Leon <leon.irenejanet@gmail.com>
2018-09-13 09:57:19 -0700

    created put method on tags

--- routes/tags.js
+++ routes/tags.js
@@ -21,13 +21,35 @@ router.get('/:id', (req,res,next) =>{
   knex.select()
     .from('tags')
     .where({id: `${tagID}`})
-    .then(results=> res.json(results))
+    .then(results=> res.json(results[0]))
     .catch(err => {
       next(err);
     });
 });
 
 
+//========UPDATE TAG NAME=============
+router.put('/:id', (req,res,next) =>{
+  const tagID = req.params.id;
+  const {name}= req.body;
+
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+
+  knex.select()
+    .from('tags')
+    .where({id: `${tagID}`})
+    .update({'name' : `${name}`})
+    .returning(['id','name'])
+    .then(results=> res.json(results[0]))
+    .catch(err => {
+      next(err);
+    });
+});
 
 
 module.exports = router;
