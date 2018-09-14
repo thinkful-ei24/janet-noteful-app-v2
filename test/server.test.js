62c3d2850a86bc39f2a399dfee13587879a81bad
Janet Leon <leon.irenejanet@gmail.com>
2018-09-14 14:46:52 -0700

    updated server test file to make sure body is an array of objects

--- test/server.test.js
+++ test/server.test.js
@@ -45,8 +45,66 @@ describe('Noteful API', function () {
     return knex.destroy(); // destroy the connection
   });
   
-  describe('GET /api/notes', function () {
+  //   describe('GET /api/notes', function () {
   
+  //     it('should return the default of 10 Notes ', function () {
+  //       return chai.request(app)
+  //         .get('/api/notes')
+  //         .then(function (res) {
+  //           expect(res).to.have.status(200);
+  //           expect(res).to.be.json;
+  //           expect(res.body).to.be.a('array');
+  //           expect(res.body).to.have.length(10);
+  //         });
+  //     });
+  
+  //     it('should return correct search results for a valid query', function () {
+  //       return chai.request(app)
+  //         .get('/api/notes?searchTerm=about%20cats')
+  //         .then(function (res) {
+  //           expect(res).to.have.status(200);
+  //           expect(res).to.be.json;
+  //           expect(res.body).to.be.a('array');
+  //           expect(res.body).to.have.length(4);
+  //           expect(res.body[0]).to.be.an('object');
+  //         });
+  //     });
+  
+  //   });
+  
+  // });
+
+  //=========404============
+  describe('404 handler', function () {
+
+    it('should respond with 404 when given a bad path', function () {
+      return chai.request(app)
+        .get('/api/')
+        .then(function (res) {
+          expect(res).to.have.status(404);
+          expect(res).to.be.json;
+          expect(res.body.message).to.equal('Not Found');
+        });
+    });
+
+
+    it('should respond with 404 when given a bad path', function () {
+      return chai.request(app)
+        .get('/api/note')
+        .then(function (res) {
+          expect(res).to.have.status(404);
+          expect(res).to.be.json;
+          expect(res.body.message).to.equal('Not Found');
+        });
+    });
+
+  });
+
+
+  //=========GET api/notes============
+  describe('GET /api/notes', function () {
+
+
     it('should return the default of 10 Notes ', function () {
       return chai.request(app)
         .get('/api/notes')
@@ -57,104 +115,75 @@ describe('Noteful API', function () {
           expect(res.body).to.have.length(10);
         });
     });
-  
-    it('should return correct search results for a valid query', function () {
+
+    it('should return an array of objects where each item contains id, title, and content', function () {
       return chai.request(app)
-        .get('/api/notes?searchTerm=about%20cats')
+        .get('/api/notes')
         .then(function (res) {
           expect(res).to.have.status(200);
           expect(res).to.be.json;
           expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length(4);
-          expect(res.body[0]).to.be.an('object');
+          const expectedKeys = ['id', 'title', 'content'];
+          res.body.forEach(function(note){
+            expect(note).to.be.a('object');
+          });
         });
     });
-  
-  });
-  
-});
 
 
-describe('404 handler', function () {
+    it('should return an empty array for an incorrect searchTerm', function () {
 
-  it('should respond with 404 when given a bad path', function () {
-    return chai.request(app)
-      .get('/api/')
-      .then(function (res) {
-        expect(res).to.have.status(404);
-        expect(res).to.be.json;
-        expect(res.body.message).to.equal('Not Found');
-      });
-  });
-
-
-  it('should respond with 404 when given a bad path', function () {
-    return chai.request(app)
-      .get('/api/note')
-      .then(function (res) {
-        expect(res).to.have.status(404);
-        expect(res).to.be.json;
-        expect(res.body.message).to.equal('Not Found');
-      });
-  });
-
-});
-
-describe('GET /api/notes', function () {
-
-  it('should return an array of objects where each item contains id, title, and content', function () {
+    });
 
   });
 
-  it('should return an empty array for an incorrect searchTerm', function () {
+  //=========GET api/:id============
+
+  describe('GET /api/notes/:id', function () {
+
+    it('should return correct note when given an id', function () {
+
+    });
+
+    it('should respond with a 404 for an invalid id', function () {
+
+    });
 
   });
 
-});
+  describe('POST /api/notes', function () {
 
-describe('GET /api/notes/:id', function () {
+    it('should create and return a new item when provided valid data', function () {
 
-  it('should return correct note when given an id', function () {
+    });
 
-  });
+    it('should return an error when missing "title" field', function () {
 
-  it('should respond with a 404 for an invalid id', function () {
-
-  });
-
-});
-
-describe('POST /api/notes', function () {
-
-  it('should create and return a new item when provided valid data', function () {
+    });
 
   });
 
-  it('should return an error when missing "title" field', function () {
+  describe('PUT /api/notes/:id', function () {
+
+    it('should update the note', function () {
+
+    });
+
+    it('should respond with a 404 for an invalid id', function () {
+
+    });
+
+    it('should return an error when missing "title" field', function () {
+
+    });
 
   });
 
-});
+  describe('DELETE  /api/notes/:id', function () {
 
-describe('PUT /api/notes/:id', function () {
+    it('should delete an item by id', function () {
 
-  it('should update the note', function () {
-
-  });
-
-  it('should respond with a 404 for an invalid id', function () {
-
-  });
-
-  it('should return an error when missing "title" field', function () {
-
-  });
-
-});
-
-describe('DELETE  /api/notes/:id', function () {
-
-  it('should delete an item by id', function () {
+    });
 
   });
 
